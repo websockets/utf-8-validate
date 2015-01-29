1cf56ec1965a43eebf9d6084c5a9ac6a8f345ef1
Arnout Kazemier <info@3rd-Eden.com>
2015-01-29 11:01:10 +0100

    [fix] Include missing fallback

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('bindings')('validation');
+'use strict';
+
+try {
+  module.exports = require('bindings')('validation');
+} catch (e) {
+  module.exports = require('./fallback');
+}
