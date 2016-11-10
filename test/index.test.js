f381774c41184b1e9dc528430accc183513da617
Luigi Pinca <luigipinca@gmail.com>
2016-11-10 16:25:05 +0100

    [test] Fix tests for node 0.12

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const assert = require('assert');
-const path = require('path');
-const fs = require('fs');
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
 
-const txt = fs.readFileSync(path.join(__dirname, 'fixtures', 'lorem-ipsum.txt'));
+var txt = fs.readFileSync(path.join(__dirname, 'fixtures', 'lorem-ipsum.txt'));
 
 describe('bindings', function() {
-  const Validation = require('bindings')('validation').Validation;
+  var Validation = require('bindings')('validation').Validation;
 
   it('should return true for a valid utf8 string', function () {
-    assert.strictEqual(Validation.isValidUTF8(Buffer.from(txt)), true);
+    assert.strictEqual(Validation.isValidUTF8(new Buffer(txt)), true);
   });
 
   it('should return false for an erroneous string', function() {
-    const invalid = Buffer.from([
+    var invalid = new Buffer([
       0xce, 0xba, 0xe1, 0xbd, 0xb9, 0xcf, 0x83, 0xce, 0xbc, 0xce, 0xb5, 0xed,
       0xa0, 0x80, 0x65, 0x64, 0x69, 0x74, 0x65, 0x64
     ]);
@@ -24,32 +24,32 @@ describe('bindings', function() {
 
   it('should return true for valid cases from the autobahn test suite', function () {
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from('\xf0\x90\x80\x80')),
+      Validation.isValidUTF8(new Buffer('\xf0\x90\x80\x80')),
       true
     );
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from([0xf0, 0x90, 0x80, 0x80])),
+      Validation.isValidUTF8(new Buffer([0xf0, 0x90, 0x80, 0x80])),
       true
     );
   });
 
   it('should return false for erroneous autobahn strings', function () {
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from([0xce, 0xba, 0xe1, 0xbd])),
+      Validation.isValidUTF8(new Buffer([0xce, 0xba, 0xe1, 0xbd])),
       false
     );
   });
 });
 
 describe.skip('fallback', function () {
-  const Validation = require('../fallback').Validation;
+  var Validation = require('../fallback').Validation;
 
   it('should return true for a valid utf8 string', function () {
-    assert.strictEqual(Validation.isValidUTF8(Buffer.from(txt)), true);
+    assert.strictEqual(Validation.isValidUTF8(new Buffer(txt)), true);
   });
 
   it('should return false for an erroneous string', function() {
-    const invalid = Buffer.from([
+    var invalid = new Buffer([
       0xce, 0xba, 0xe1, 0xbd, 0xb9, 0xcf, 0x83, 0xce, 0xbc, 0xce, 0xb5, 0xed,
       0xa0, 0x80, 0x65, 0x64, 0x69, 0x74, 0x65, 0x64
     ]);
@@ -59,18 +59,18 @@ describe.skip('fallback', function () {
 
   it('should return true for valid cases from the autobahn test suite', function () {
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from('\xf0\x90\x80\x80')),
+      Validation.isValidUTF8(new Buffer('\xf0\x90\x80\x80')),
       true
     );
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from([0xf0, 0x90, 0x80, 0x80])),
+      Validation.isValidUTF8(new Buffer([0xf0, 0x90, 0x80, 0x80])),
       true
     );
   });
 
   it('should return false for erroneous autobahn strings', function () {
     assert.strictEqual(
-      Validation.isValidUTF8(Buffer.from([0xce, 0xba, 0xe1, 0xbd])),
+      Validation.isValidUTF8(new Buffer([0xce, 0xba, 0xe1, 0xbd])),
       false
     );
   });
