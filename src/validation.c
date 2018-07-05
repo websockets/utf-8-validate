a3e2ac4112ae4e58e46b9655ce64489cca22ebad
Luigi Pinca <luigipinca@gmail.com>
2018-07-05 08:23:04 +0200

    [minor] Remove license comments

--- src/validation.c
+++ src/validation.c
@@ -1,9 +1,3 @@
-/*!
- * UTF-8 validate: UTF-8 validation for WebSockets.
- * Copyright(c) 2015 Einar Otto Stangvik <einaros@gmail.com>
- * MIT Licensed
- */
-
 #include <assert.h>
 #include <node_api.h>
 
